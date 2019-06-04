f6ba09f8ff93047a6364baaa607d373c294bdd81
petraartep <petra.pogacsas@gmail.com>
2019-06-04 12:58:27 +0100

    completely refactored

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,28 +1,33 @@
 class Oystercard
+
+  MAXIMUM_BALANCE = 90
   
-  MAXBALANCE = 90
-  attr_reader :balance, :in_journey
-    alias :in_journey? :in_journey
-  
+  attr_reader :balance #, :in_journey
+  #alias :in_journey? :in_journey
+
   def initialize
     @balance = 0
     @in_journey = false
   end
 
-  def top_up(value)
-    raise "Reached limit £#{MAXBALANCE}" if @balance + value >= MAXBALANCE
-    @balance = @balance + value
+  def top_up(amount)
+    fail 'Maximum balance of #{maximum_balance} exceeded' if amount + balance > MAXIMUM_BALANCE
+    @balance += amount
   end
 
-  def deduct(value)
-    @balance = @balance - value
+  def deduct(amount)
+    @balance -= amount
+  end
+
+  def in_journey?
+    @in_journey
   end
 
   def touch_in
     @in_journey = true
   end
-
-   def touch_out
+  
+  def touch_out
     @in_journey = false
   end
 end
